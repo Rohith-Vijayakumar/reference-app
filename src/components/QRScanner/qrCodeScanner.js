a92bd5faaf44ce6f0981f59a4123869ec595e8ad
Gajask <mail2gajask@gmail.com>
2022-06-14 09:47:58 +0530

    QR Scanner updated layout

--- src/components/QRScanner/qrCodeScanner.js
+++ src/components/QRScanner/qrCodeScanner.js
@@ -1,27 +1,58 @@
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import { RNCamera } from 'react-native-camera';
 
-import { Text, View } from 'react-native'
+import {AppRegistry,Linking} from 'react-native'
 import React, { Component } from 'react'
 
 export default class QRScanner extends Component {
+  onSuccess = e => {
+    Linking.openURL(e.data).catch(err =>
+      console.error('An error occured', err)
+    );
+  };
 
-state ={
-    qr: ""
-}
-onRead = e => {
-    this.setState({ qr: e.data})
-}
   render() {
     return (
-      <View>
-        <QRCodeScanner onRead={this.onRead}/>
-        <View>
-           <Text>{this.state.qr}</Text>
-          </View>
-      </View>
-     
-    )
+      <QRCodeScanner
+        onRead={this.onSuccess}
+        flashMode={RNCamera.Constants.FlashMode.torch}
+        // topContent={
+        //   <Text style={styles.centerText}>
+        //     Go to{' '}
+        //     <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+        //     your computer and scan the QR code.
+        //   </Text>
+        // }
+        // bottomContent={
+        //   <TouchableOpacity style={styles.buttonTouchable}>
+        //     <Text style={styles.buttonText}>OK. Got it!</Text>
+        //   </TouchableOpacity>
+        // }
+      />
+    );
   }
 }
+
+// const styles = StyleSheet.create({
+//   centerText: {
+//     flex: 1,
+//     fontSize: 18,
+//     padding: 32,
+//     color: '#777'
+//   },
+//   textBold: {
+//     fontWeight: '500',
+//     color: '#000'
+//   },
+//   buttonText: {
+//     fontSize: 21,
+//     color: 'rgb(0,122,255)'
+//   },
+//   buttonTouchable: {
+//     padding: 16
+//   }
+// });
+
+AppRegistry.registerComponent('default', () => ScanScreen);
+
 
